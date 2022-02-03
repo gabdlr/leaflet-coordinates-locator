a23e37c144355878fb57b0c426312d949283987f
Gabriel De Los Rios <gab.delosrios@gmail.com>
2022-02-03 15:59:10 -0300

    1st commit

---
+++ map.js
@@ -0,0 +1,36 @@
+var map = L.map('map').setView([-34.6037389, -58.3815704], 11);
+
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors'
+}).addTo(map);
+
+var popup = L.popup();
+
+let searchButton = document.getElementById('search');
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Has hecho click en la coordenada:<br> " +  e.latlng.lat.toString() + "," +  e.latlng.lng.toString()) // Sets the HTML content of the popup.
+        .openOn(map);
+}
+
+function goTo(){
+    let lat = parseFloat(document.getElementById('lat').value);
+    let lng = parseFloat(document.getElementById('lng').value);
+    if(isNaN(lat) ||isNaN(lng)){
+        alert("Debe ingresar ambas coordenadas en un formato válido");
+        return;
+    }
+    map.setView([lat,lng], 11);
+    popup
+    .setLatLng({lat,lng})
+    .setContent("Ubicación")
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
+searchButton.addEventListener('click', () => {goTo()});
+
+
+
